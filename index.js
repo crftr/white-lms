17502bf9f510f024173249d7e36034af85be4bed
Mike Herrera <michael.herrera@gmail.com>
2022-09-17 16:31:04 -0700

    Table presentation

--- index.js
+++ index.js
@@ -1,33 +1,6 @@
 import prettyjson from 'prettyjson';
 import readXlsxFile from 'read-excel-file/node';
 
-let players = {};
-
-readXlsxFile('lms.xlsx').then((arrayRows) => {
-
-    arrayRows = cleanNullsFromNestedArrays(arrayRows);
-    arrayRows = isolateThePlayerRows(arrayRows);
-
-    arrayRows.forEach(playerRow => {
-        let pName;
-        const regEx_player = /(?<name>[\w\d\ \.-]+(?<! ))\s*(?<paid>\*{0,2})/g;
-        
-        for (const match of playerRow[0].matchAll(regEx_player)) {
-            pName = match.groups?.name.replace(/\s+/g, ' ');
-            players[pName] = {
-                name: pName,
-                paid: (match.groups?.paid || '').length,
-            }
-        }
-
-        const playerGames = playerRow.slice(1).map(game => game.trim().toUpperCase());
-        players[pName].games = playerGames;
-    });
-
-    console.log(prettyjson.render(players));
-
-})
-
 function cleanNullsFromNestedArrays(arrayOfArrays) {
     return arrayOfArrays.map((row) => row.filter((rowElement) => rowElement !== null));
 }
@@ -39,3 +12,52 @@ function isolateThePlayerRows(rows) {
         return validLength && !isHeaderRow;
     });
 }
+
+let players = [];
+
+readXlsxFile('lms.xlsx').then((arrayRows) => {
+    arrayRows = cleanNullsFromNestedArrays(arrayRows);
+    arrayRows = isolateThePlayerRows(arrayRows);
+    arrayRows.forEach(playerRow => {
+        const regEx_player = /(?<name>[\w\d\ \.-]+(?<! ))\s*(?<asterisks>\*{0,2})/g;
+        let pName, player;
+        
+        // Column A, player name and count of entries
+        //
+        for (const match of playerRow[0].matchAll(regEx_player)) {
+            pName = match.groups?.name.replace(/\s+/g, ' ');
+            player = {
+                name: pName,
+                paidFor: (match.groups?.asterisks || '').length,
+            }
+        }
+
+        // Column B through S, Weekly choices
+        //
+        const playerGames = playerRow.slice(1).map(game => game.trim().toUpperCase());
+        player.games = playerGames;
+        player.gamesEntered = playerGames.length;
+        player.gamesAreValid = (new Set(playerGames)).size === playerGames.length;
+
+        players.push(player);
+    });
+
+    // console.log(prettyjson.render(playersObj));
+
+
+    players.sort((p1, p2) => {
+        if (!p1.gamesAreValid && p2.gamesAreValid) return -1;
+        if (p1.gamesAreValid && !p2.gamesAreValid) return 1;
+
+        if (p1.gamesEntered > p2.gamesEntered) return -1;
+        if (p1.gamesEntered < p2.gamesEntered) return 1;
+
+        if (p1.name < p2.paidFor) return -1;
+        if (p1.name > p2.paidFor) return 1;
+
+        return 0;
+    })
+
+    console.table(players);
+
+});
